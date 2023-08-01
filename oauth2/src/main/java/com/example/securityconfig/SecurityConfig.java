7ebd1fecd65cbc75cbe932a72116dcbd67058cd3
nikkhil-ss <nikhil.singh_cs19@gla.ac.in>
2023-08-01 21:35:49 +0530

    new files added

---
+++ oauth2/src/main/java/com/example/securityconfig/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth->{
+		auth.requestMatchers("/home").permitAll();
+		auth.anyRequest().authenticated();
+				
+		})
+		.oauth2Login(Customizer.withDefaults())
+		.formLogin(Customizer.withDefaults());
+		
+		
+		return http.build();
+		
+	}
+	
+}
